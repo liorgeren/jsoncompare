9890026e32be6e0063d5ad66b4547fbc937a38a9
railesax <railesax@adobe.com>
2015-12-02 17:01:00 -0700

    trying out a change to __init__.py

--- json_compare/__init__.py
+++ json_compare/__init__.py
@@ -1,1 +1,2 @@
-from .json_compare import are_same, json_are_same, contains
+from json_compare import are_same, contains, json_are_same
+__all__ = ['are_same', 'contains', 'json_are_same']
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_readme(filename):
 
 setup(
     name='json_compare',
-    version='0.2.3',
+    version='0.2.4',
     description='Json comparison tool',
     author='Chris Saxey',
     author_email='chrissaxey@gmail.com',
