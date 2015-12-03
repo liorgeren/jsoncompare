247d7de159b5c98da6e76c094674f42a162b3f86
railesax <railesax@adobe.com>
2015-12-02 17:03:35 -0700

    another __init__ change. Plus using travis is fun.

--- json_compare/__init__.py
+++ json_compare/__init__.py
@@ -1,2 +1,2 @@
-from json_compare import are_same, contains, json_are_same
+from .json_compare import are_same, contains, json_are_same
 __all__ = ['are_same', 'contains', 'json_are_same']
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_readme(filename):
 
 setup(
     name='json_compare',
-    version='0.2.4',
+    version='0.2.5',
     description='Json comparison tool',
     author='Chris Saxey',
     author_email='chrissaxey@gmail.com',
