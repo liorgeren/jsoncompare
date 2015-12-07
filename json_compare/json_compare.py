a02b2181282e6c0c8348289dd760ec8c49b78548
railesax <railesax@adobe.com>
2015-12-07 16:09:12 -0700

    import fix again

--- json_compare/json_compare.py
+++ json_compare/json_compare.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 if sys.version_info[0] < 3:
     from future import range
 if sys.version_info[0] == 3 and sys.version_info[1] > 5:
-    from is_close import isclose
+    from .is_close import isclose
 else:
     from math import isclose
 
